9979e44bb9a463fba136be60d2acce2c14450154
Kyle <kixzxz97@gmail.com>
2017-05-11 00:54:10 +0800

    commit

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,6 +13,8 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+Vue.component('chat-messages', require('./components/ChatMessages.vue'));
+Vue.component('chat-form', require('./components/ChatForm.vue'));
 
 const app = new Vue({
     el: '#app',

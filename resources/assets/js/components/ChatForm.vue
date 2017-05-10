17271d447133fe69e2aa6c67aae7e93214a8f95c
Kyle <kixzxz97@gmail.com>
2017-05-11 00:53:39 +0800

    commit

---
+++ resources/assets/js/components/ChatForm.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="input-group">
+        <input id="btn-input" type="text" name="message" class="form-control input-sm" placeholder="Type your message here..." v-model="newMessage" @keyup.enter="sendMessage">
+        
+        <span class="input-group-btn">
+            @if($recipient)
+            <button class="btn btn-primary btn-sm" id="btn-chat" @click="sendMessage"> Private </button>
+            @else
+            <button class="btn btn-primary btn-sm" id="btn-chat" @click="sendMessage"> Public </button>
+            @endif
+                
+        </span>
+    </div>
+</template>
+
+<script>    
+    export default {
+        props: ['user','recipient'],
+
+        data() {
+            return {
+                newMessage: ''
+            }
+        },
+
+        methods: {
+            sendMessage() {
+                this.$emit('messagesent', {
+                    user: this.user,
+                    message: this.newMessage
+                });
+
+                this.newMessage = ''
+            }
+        }    
+    }
+</script>

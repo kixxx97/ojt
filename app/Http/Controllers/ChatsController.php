176ae92ac16ab2badd95ee0627054b28995b3ab9
kixxx97 <kixzxz97@gmail.com>
2017-05-24 00:28:27 +0800

    chats controller commit

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Mail\SendMessage;
 
-class ChatsController extends Controller
+<!--  -->lass ChatsController extends Controller
 
 {
     /**
